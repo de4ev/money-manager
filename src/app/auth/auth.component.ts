145b4c5ae1d34fcf39b8ca1bc404b2b9ad5d2b60
de4ev <voldemort_001@mail.ru>
2018-07-23 23:56:09 +0300

    auth module

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "../../../node_modules/@angular/core";
+
+@Component({
+    selector: 'mm-auth',
+    templateUrl: './auth.component.html'
+})
+export class AuthComponent {}
---
+++ src/app/auth/registration/registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mm-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.sass']
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
