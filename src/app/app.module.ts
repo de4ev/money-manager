2c9375e2407997c6baf25123c046417c7e3d9343
de4ev <voldemort_001@mail.ru>
2018-07-24 23:51:21 +0300

    auth module routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AuthModule } from './auth/auth.module';
   ],
   imports: [
     BrowserModule,
-    AuthModule
+    AuthModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component'
+import { RegistrationComponent } from './registration/registration.component';
 import { AuthComponent } from './auth.component';
-import { CommonModule } from '../../../node_modules/@angular/common';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
     declarations: [
-        LoginComponent, 
+        LoginComponent,
         RegistrationComponent,
         AuthComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        AppRoutingModule
     ]
 })
-
 export class AuthModule {}
