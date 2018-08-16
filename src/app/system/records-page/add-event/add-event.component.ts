53ae02594d28df8eceefbe88ed9a8fc8a3166d06
de4ev <voldemort_001@mail.ru>
2018-08-16 15:12:28 +0300

    records page component

---
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mm-add-event',
+  templateUrl: './add-event.component.html',
+  styleUrls: ['./add-event.component.sass']
+})
+export class AddEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
